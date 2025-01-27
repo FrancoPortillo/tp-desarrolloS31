d21984849bcd51c9bd2a66b348b44e07107d6972
Santiago <santicanu4@gmail.com>
2025-01-26 21:36:14 -0300

    .

--- backend/Servicios/obj/Debug/net8.0/Servicios.AssemblyInfo.cs
+++ backend/Servicios/obj/Debug/net8.0/Servicios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3453c5159e6f14bf518d1020decdb43cb968140e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05519bf81de80a41e22261a8644edde0af25896d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
