0570fae292118ccc7717868d4a153ecb5fe18354
Santiago <santicanu4@gmail.com>
2025-01-27 16:48:45 -0300

    .

--- backend/DataBase/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ backend/DataBase/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d73112f90d0493f429315862b93887b57f0cf67f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72859638dae3620ded30ef2d5e85fc9fd0aa708d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/Servicios/obj/Debug/net8.0/Servicios.AssemblyInfo.cs
+++ backend/Servicios/obj/Debug/net8.0/Servicios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d73112f90d0493f429315862b93887b57f0cf67f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72859638dae3620ded30ef2d5e85fc9fd0aa708d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Servicios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
