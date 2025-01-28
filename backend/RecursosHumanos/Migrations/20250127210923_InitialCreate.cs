14aeb23107700dcaee49755869c7bb35110517e2
Santiago <santicanu4@gmail.com>
2025-01-28 01:41:00 -0300

    .

---
+++ backend/RecursosHumanos/Migrations/20250127210923_InitialCreate.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RecursosHumanos.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresa",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Telefono = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Direccion = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresa", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleado",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Apellido = table.Column<string>(type: "text", nullable: false),
+                    Legajo = table.Column<int>(type: "integer", nullable: false),
+                    Edad = table.Column<int>(type: "integer", nullable: false),
+                    Dni = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Puesto = table.Column<string>(type: "text", nullable: false),
+                    Rol = table.Column<string>(type: "text", nullable: false),
+                    IdEmpresa = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleado", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Empleado_Empresa_IdEmpresa",
+                        column: x => x.IdEmpresa,
+                        principalTable: "Empresa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Asistencia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Presente = table.Column<bool>(type: "boolean", nullable: false),
+                    IdEmpleado = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asistencia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Asistencia_Empleado_IdEmpleado",
+                        column: x => x.IdEmpleado,
+                        principalTable: "Empleado",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LLegadaTarde",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Motivo = table.Column<string>(type: "text", nullable: false),
+                    MinutosTarde = table.Column<int>(type: "integer", nullable: false),
+                    IdEmpleado = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LLegadaTarde", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LLegadaTarde_Empleado_IdEmpleado",
+                        column: x => x.IdEmpleado,
+                        principalTable: "Empleado",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vacaciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FechaInicio = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FechaFin = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Aprobado = table.Column<bool>(type: "boolean", nullable: false),
+                    IdEmpleado = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacaciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vacaciones_Empleado_IdEmpleado",
+                        column: x => x.IdEmpleado,
+                        principalTable: "Empleado",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Asistencia_IdEmpleado",
+                table: "Asistencia",
+                column: "IdEmpleado");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empleado_IdEmpresa",
+                table: "Empleado",
+                column: "IdEmpresa");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LLegadaTarde_IdEmpleado",
+                table: "LLegadaTarde",
+                column: "IdEmpleado");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacaciones_IdEmpleado",
+                table: "Vacaciones",
+                column: "IdEmpleado");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asistencia");
+
+            migrationBuilder.DropTable(
+                name: "LLegadaTarde");
+
+            migrationBuilder.DropTable(
+                name: "Vacaciones");
+
+            migrationBuilder.DropTable(
+                name: "Empleado");
+
+            migrationBuilder.DropTable(
+                name: "Empresa");
+        }
+    }
+}
